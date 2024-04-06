2c351b27f9c2ad86e66235d7777f2f0414f4e3ee
Elian Camilo Salinas Casas <camilo4829@hotmail.com>
2024-04-05 22:47:52 -0500

    Carga de los conjuntos de datos

--- Icfes/Saber11/Codigos/Preprocesamiento.R
+++ Icfes/Saber11/Codigos/Preprocesamiento.R
@@ -17,3 +17,22 @@ library(plotly)
 
 # CONJUNTOS DE DATOS -----------------------------------------------------------
 
+#file.choose()
+
+# Datos Saber 11 - 2023 1
+saber20231 <- data.frame(read_delim("C:\\Users\\camil\\Documents\\StatisticsLocal\\R-Proyectos\\Icfes\\Saber11\\Datasets\\SB11_20231.TXT",
+                                    delim = "¬"))
+
+# Datos Saber 11 - 2023 2
+saber20232 <- data.frame(read_delim("C:\\Users\\camil\\Documents\\StatisticsLocal\\R-Proyectos\\Icfes\\Saber11\\Datasets\\SB11_20232.TXT",
+                                    delim = "¬"))
+
+datos <- c("saber20231", "saber20232")
+
+sapply(datos, function(i) dim(get(i)))
+
+
+
+
+
+
