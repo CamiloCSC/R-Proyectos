d2495752d8b2141048afa9970a7fa2bec1a5cc19
Elian Camilo Salinas Casas <camilo4829@hotmail.com>
2024-04-05 22:29:44 -0500

    Adición del codigo de preprocesamiento, directorio y librerias principales

---
+++ Icfes/Saber11/Codigos/Preprocesamiento.R
@@ -0,0 +1,19 @@
+# DIRECTORIO -------------------------------------------------------------------
+
+setwd("C:/Users/camil/Documents/StatisticsLocal/R-Proyectos/Icfes/Saber11/Codigos")
+
+
+# LIBRERIAS --------------------------------------------------------------------
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(stringr)
+library(naniar)
+library(ggplot2)
+library(plotly)
+
+
+# CONJUNTOS DE DATOS -----------------------------------------------------------
+
